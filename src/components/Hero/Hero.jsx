116f8e79ce2c1e2cd06aa6847da5d28278869a63
SahilChandravanshi <sahilcdelhi@gmail.com>
2022-10-01 07:35:41 +0530

    hero section added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./Hero.css";
+import logo from "../../images/logo.png";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+	return (
+		<div className="hero_container colCenter">
+			<div class="logo">
+				<img src={logo} alt="Logo" />
+			</div>
+			<div className="desc colCenter">
+				<h1>Saklash Theme</h1>
+				<p>The theme you will ever need!</p>
+				<p>
+					Minimal, friendly theme for VS code, Sublime Text, Windows Terminal.
+				</p>
+			</div>
+		</div>
+	);
+};
 
 export default Hero;
