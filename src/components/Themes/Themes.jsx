0824bcb114e862cd43a0f293c7f454784e24d8d7
sahil chandravanshi <sahilcdelhi@gmail.com>
2022-10-06 05:53:32 +0530

    theme change functionality added

--- src/components/Themes/Themes.jsx
+++ src/components/Themes/Themes.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import colorTheme from "../../colorThemes.json"
 import "./Themes.css";
 
 const Themes = () => {
+
+	const [selected, setSelected] = useState(colorTheme[0].image)
+
 	return (
 		<div className="theme_container rowCenter">
 			<div className="theme_selector">
-				<span class="square s1"></span>
-				<span class="square s2"></span>
-				<span class="square s3"></span>
-				<span class="square s4"></span>
+				{colorTheme.map(constant => {
+					return (
+						<span className="square" style={{"background-color": constant.color}} onClick={() => setSelected(constant.image)}></span>
+					)
+				})}
 			</div>
 			<div className="theme_image">
-				<img src={require("../../images/SOOTHINGt.png")} alt="" />
+						<img src={selected} alt="" />
 			</div>
 		</div>
 	);
